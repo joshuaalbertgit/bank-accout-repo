29325b868e82c652bedfc1408b679b1c9631854f
joshuaalbertgit <joshuaalbert@outlook.com>
2024-04-10 18:43:04 +0530

    adding the last controller (AccountLookupController)/API (/api/v1/bankAccountLookup")

--- cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/QueryDispatcher.java
+++ cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/QueryDispatcher.java
@@ -8,9 +8,10 @@ import java.util.List;
 
 public interface QueryDispatcher {
     //have two methods
-    // 1. registerHandler - which uses /queries/QueryHandlerMethod<T extends BaseQuery>
-    // 2. Send - which uses /queries/abstract class BaseQuery
 
+    // 1. registerHandler - which uses /queries/QueryHandlerMethod<T extends BaseQuery>
     <T extends BaseQuery> void registerHandler(Class<T> type, QueryHandlerMethod<T> handler);
+
+    // 2. Send - which uses /queries/abstract class BaseQuery
     <U extends BaseEntity>List<U> send(BaseQuery query);
 }
